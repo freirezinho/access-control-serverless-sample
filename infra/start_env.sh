dd5eaa481ac593cfcf6d1c975feeef97617942d2
Saulo Freire <saulo.freire@petzpartner.com.br>
2023-05-23 22:21:21 -0300

    chore(infra): delete old terraform related files

--- infra/start_env.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-cd /home/localstack/app/infra
-terraform init
-terraform apply --auto-approve
--- src/lambda.py
+++
@@ -1,4 +0,0 @@
-import uuid
-
-def get_key(event, context):
-    return { "key": str(uuid.uuid5(uuid.NAMESPACE_X500, 'key')) }
