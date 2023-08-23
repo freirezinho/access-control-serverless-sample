d3fbdad2660d04ddb08ff12de7ae9b522aff7dfa
Saulo Freire <saulo.freire@petzpartner.com.br>
2023-08-23 16:20:11 -0300

    feat(ci): update script

--- deploy.sh
+++ deploy.sh
@@ -16,3 +16,15 @@ else
   sed -i'' "s/=\s'db'/= '$DB_HOST'/g" ./api-gtw-acck/handler.py
   sed -i'' 's/'yippy'/'$DB_NAME'/g' ./api-gtw-acck/handler.py
 fi
+
+if [[ $(uname) = "Darwin" ]]; then
+  sed -i '' 's/'hippy'/'$DB_USER'/g' ./api-gtw-acck/logs_handler.py
+  sed -i '' 's/'pippy'/'$DB_PASSWORD'/g' ./api-gtw-acck/logs_handler.py
+  sed -i '' "s/=[[:space:]]'db'/= '$DB_HOST'/g" ./api-gtw-acck/logs_handler.py
+  sed -i '' 's/'yippy'/'$DB_NAME'/g' ./api-gtw-acck/logs_handler.py
+else
+  sed -i'' 's/'hippy'/'$DB_USER'/g' ./api-gtw-acck/logs_handler.py
+  sed -i'' 's/'pippy'/'$DB_PASSWORD'/g' ./api-gtw-acck/logs_handler.py
+  sed -i'' "s/=\s'db'/= '$DB_HOST'/g" ./api-gtw-acck/logs_handler.py
+  sed -i'' 's/'yippy'/'$DB_NAME'/g' ./api-gtw-acck/logs_handler.py
+fi
